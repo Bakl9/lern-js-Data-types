14eab156e1caa46b41fb9845220325aa783927b6
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-22 18:04:44 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+Number - var number = 42 
+string - var string = 'massage'
+Boolean - var isTrue = true/false
+Object - var obj = {a:1}
+Null - var isNull = null
+Undefined var = undef = undefined
+/*
